608effab020ddd12b493fc0a25ffc477b28f14c2
Shabana Dhayananth <dhayanas@mcmaster.ca>
2018-01-28 07:21:03 -0500

    fixed url

--- get_playlist.py
+++ get_playlist.py
@@ -8,14 +8,20 @@ def get_playlist():
         'Authorization': 'Bearer BQAawDQpEsHRfWmfQAS2rBICU7fomzo4kxe2Hc2W0JP8KP9I_r4zo_3vfy62j_AiRgh5_ax9skkrGln0YXO5ndRx3j327SMKQgUKEXLMJhHfhxTuU-4oklNGBDlRkZ71qdXNVryo4WJHTBa6iEZsIqjOJfRmu6eMQA-bflvbp_pYDE0Tr49RVCbHIYVJQy5Xu4EGvJtdZ-Q590B-T13T-ao',
     }
 
+    url = ""
+
     if (emotion == "joy"):
-        response = requests.get("https://api.spotify.com/v1/users/spotify/playlists/37i9dQZF1DXdPec7aLTmlC", headers = headers)
+        url = "https://open.spotify.com/embed?uri=spotify:user:spotify:playlist:37i9dQZF1DXdPec7aLTmlC" 
     elif (emotion == "anger"):
-        response = requests.get("https://api.spotify.com/v1/users/spotify/playlists/5s7Sp5OZsw981I2OkQmyrz", headers = headers)
+        url = "https://open.spotify.com/embed?uri=spotify:user:spotify:playlist:5s7Sp5OZsw981I2OkQmyrz"
     elif (emotion == "fear"):
-        response = requests.get("https://api.spotify.com/v1/users/spotify/playlists/5fnM0FdCpinIGWnGGPLkVG", headers = headers)
+        url = "https://open.spotify.com/embed?uri=spotify:user:spotify:playlist:5fnM0FdCpinIGWnGGPLkVG"
     elif (emotion == "sadness"):
-        response = requests.get("https://api.spotify.com/v1/users/spotify/playlists/37i9dQZF1DWVV27DiNWxkR", headers = headers)
+        url = "https://open.spotify.com/embed?uri=spotify:user:spotify:playlist:37i9dQZF1DWVV27DiNWxkR"
     elif (emotion == "surprise"):
-        response = requests.get("https://api.spotify.com/v1/users/spotify/playlists/17KsY3IOMquuLpi1h6bk9j", headers = headers)
-    return response
+        url = "https://open.spotify.com/embed?uri=spotify:user:spotify:playlist:17KsY3IOMquuLpi1h6bk9j"
+
+    return url
+
+
+
